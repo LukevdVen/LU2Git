b851821cd793be70cbbfedbe27449d5abc9cd411
LukevandeVen <lcj.vandeven1@student.avans.nl>
2025-04-13 11:26:34 +0200

    Path fix

--- LU2GitTest/obj/Debug/net9.0/LU2GitTest.AssemblyInfo.cs
+++ LU2GitTest/obj/Debug/net9.0/LU2GitTest.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LU2GitTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8eaa60cc11ed53524364e46b8d50a3d1e39827e6")]
 [assembly: System.Reflection.AssemblyProductAttribute("LU2GitTest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LU2GitTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
